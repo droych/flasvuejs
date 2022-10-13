77b69156e3ff6219f4b1203cbc0f8f3a070df7cb
droych <115641539+droych@users.noreply.github.com>
2022-10-13 19:42:24 +0530

    upadated

--- hopwok/src/router/index.js
+++ hopwok/src/router/index.js
@@ -31,7 +31,7 @@ const router = createRouter({
       name: 'product',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      
       component: () => import('../views/Productviewid.vue'),
       props:true
     }
