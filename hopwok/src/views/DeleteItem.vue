70fdac0e891820153bfc5524aad60df575b85e3f
droych <roychaudhurydebasmita@gmail.com>
2022-10-19 22:23:20 +0530

    updated

---
+++ hopwok/src/views/DeleteItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ProductDetails from '@/components/ProductDetails.vue';
+import DeleteCart from '../components/DeleteCart.vue';
+</script>
+<template>
+    <div >
+
+      <DeleteCart  :id = "parseInt($route.params.id)" />     
+
+    </div>
+  </template>
+<style>
+  @media (min-width: 1024px) {
+    .products {
+      min-height: 20vh;
+      position: relative;
+      left: -250px; top: -250px;
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
+  
